3e301876ddb8e5e7fa4b4a18ec7d058e5b7f1e67
Azeem <azeemahmed101@gmail.com>
2020-06-06 17:13:31 +0200

    production ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles
 end
---
+++ db/migrate/20200606095300_add_timestamps_to_articles.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddTimestampsToArticles < ActiveRecord::Migration[6.0]
+  def change
+    # add_column :articles, :created_at, :datetime
+    # add_column :articles, :updated_at, :datetime
+  end
+end
