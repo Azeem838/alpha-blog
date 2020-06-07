7ed51f64cbb5c8683671944afca3adcc7d0618c2
Azeem <azeemahmed101@gmail.com>
2020-06-07 13:24:22 +0200

    fix env bundle

--- db/migrate/20200606094635_create_articles.rb
+++ db/migrate/20200606094635_create_articles.rb
@@ -2,9 +2,9 @@
 
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
-    create_table :articles do |t|
-      t.string :title
-      t.text :description
-    end
+    # create_table :articles do |t|
+    #   t.string :title
+    #   t.text :description
+    # end
   end
 end
