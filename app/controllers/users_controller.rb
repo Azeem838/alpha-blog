75ef25e71fc088bbb397d274cbf77788617b473b
Azeem <azeemahmed101@gmail.com>
2020-06-13 23:42:52 +0200

    add user edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,7 +15,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Your account information was successfully updated'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
