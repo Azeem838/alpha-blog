f2a9ddcdc96e0627d7ead2696b6bf3ad7c724904
Azeem <azeemahmed101@gmail.com>
2020-06-14 21:21:20 +0200

    add categories

---
+++ db/migrate/20200614163222_create_article_categories.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CreateArticleCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :article_categories do |t|
+      t.integer :article_id
+      t.integer :category_id
+    end
+  end
+end
